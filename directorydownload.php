2938de92d9a00b37b02ccd0297e3a7a5e9016499
fititnt <emerson@webdesign.eng.br>
2011-09-20 05:20:26 -0300

    Removed inused functions, and added a few more drafts

--- directorydownload.php
+++ directorydownload.php
@@ -15,133 +15,102 @@ jimport('joomla.plugin.plugin');
  */
 class plgContentDirectorydownload extends JPlugin
 {
-	/**
-	 * Example prepare content method
-	 *
-	 * Method is called by the view
-	 *
-	 * @param	string          The context of the content being passed to the plugin.
-	 * @param	object          The content object.  Note $article->text is also available
-	 * @param	object          The content params
-	 * @param	int		The 'page' number
-	 * @since	1.6
-	 */
-	public function onContentPrepare($context, &$article, &$params, $limitstart)
-	{
-		$app = JFactory::getApplication();
-                
-                // simple performance check to determine whether bot should process further
-                $tagname = $this->get('tagname', 'github');
-		if (strpos($article->text, $tagname) === false) {
-			return true;
-		}
-                
-		// expression to search
-                // {github}https://raw.github.com/example...{/github}
-                // @todo: rewrite to make able to ask start and end lines
-                $regex		= '~{'.$tagname.'}(.*?){/'.$tagname.'}~i'; 
-		$matches	= array();
+    /**
+     * Example prepare content method
+     *
+     * Method is called by the view
+     *
+     * @param	string          The context of the content being passed to the plugin.
+     * @param	object          The content object.  Note $article->text is also available
+     * @param	object          The content params
+     * @param	int		The 'page' number
+     * @since	1.6
+     */
+    public function onContentPrepare($context, &$article, &$params, $limitstart)
+    {
+            $app = JFactory::getApplication();
 
-		// find all instances of plugin and put in $matches
-		preg_match_all($regex, $article->text, $matches, PREG_SET_ORDER);
-
-		foreach ($matches as $match) {
-			// $match[0] is full pattern match, $match[1] is the url
-			$code = $this->_getGithubCode($match[1]);
-			// We should replace only first occurrence in order to allow positions with the same name to regenerate their content:
-			$article->text = preg_replace("|$match[0]|", $code, $article->text, 1);
-		}
-                return '';
-	}
-        
-        /* Function to get get and change the githubcode
-         * @author      Emerson Rocha Luiz
-         * @var         string          $url: the url to get. Must be RAW url!
-         * @var         int             $start: line to start to show
-         * @var         int             $end: last line to show
-         * @return      string          $github: the final github code to show
-         */
-        
-        protected function _getGithubCode($url, $start = FALSE, $end = FALSE){
-            //Get Page
-            $page = $this->_getUrlContents($url, FALSE);
-            
-            //Convert linebreaks
-            $page = $this->_Unix2Dos($page);
-            
-            //Get only desired lines
-            if($start !== FALSE || $end !== FALSE){
-                $page = $this->_getStringLines($page, $start, $end);
-            }
-            //Clean up special chars
-            $github = htmlspecialchars($page);
-            
-            //Get start and end tags and apply
-            $tagstart = $this->params->get('tagstart', '<pre>');
-            $tagstart = str_replace('&lt;', '<', $tagstart);
-            $tagstart = str_replace('&gt;', '>', $tagstart);
-            $tagsend  = $this->params->get('tagend', '</pre>');
-            $tagsend  = str_replace('&lt;', '<', $tagsend);
-            $tagsend  = str_replace('&gt;', '>', $tagsend);
-            $github   = $tagstart . $github . $tagsend;
-            
-            return $github;
-        }        
-        
-        /*
-         * Return contents of url
-         * @author      Emerson Rocha Luiz
-         * @var         string      $url
-         * @var         string      $certificate path to certificate if is https URL
-         * @return      string
-         */
-        protected function _getUrlContents($url, $certificate = FALSE){
-            $ch = curl_init(); //Inicializar a sessao           
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);//Retorne os dados em vez de imprimir em tela
-            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, $certificate);//Check certificate if is SSL, default FALSE
-            curl_setopt($ch, CURLOPT_URL, $url);//Setar URL
-            $content = curl_exec($ch);//Execute
-            curl_close($ch);//Feche          
-            
-            return $content;
-        }
-        
-        /* Return just lines betwen betwen start and end lines
-         * @author      Emerson Rocha Luiz
-         * @var         string          $string: the string to edit
-         * @var         int             $start: initial line
-         * @var         int             $end: end line
-         * @return      string
-         */
-        protected function _getStringLines($string, $start, $end){
-
-            $stringArray = explode(PHP_EOL, $string);
-            $nLines = count($stringArray)-1;
-
-            //Handle a few errors
-            if( $end < $start || $end > $nLines){
-                //return FALSE;
+            // simple performance check to determine whether bot should process further
+            $tagname = $this->get('tagname', 'github');
+            if (strpos($article->text, $tagname) === false) {
+                    return true;
             }
 
-            $result = '';
-            for( $i=($start-1); $i<=$end ; $i++ ){
-                $result .= $stringArray[$i] . PHP_EOL;
-            }    
-            return $result;    
-        }
-        
-        /* Convert unix linebreaks to windows line breaks
-         * @author      Emerson Rocha Luiz
-         * @var         string          $string: the string to edit
-         * @return      string          $newstring
-         */
-        protected function _Unix2Dos($string){
-            if (strpos($string, "\n") === false) {
-                //$newstring = false;
-                $newstring = $string;				
-            } else {
-                $newstring = str_replace("\n", "\r\n", $string); 				 
+            // expression to search
+            // {github}https://raw.github.com/example...{/github}
+            // @todo: rewrite to make able to ask start and end lines
+            $regex		= '~{'.$tagname.'}(.*?){/'.$tagname.'}~i'; 
+            $matches	= array();
+
+            // find all instances of plugin and put in $matches
+            preg_match_all($regex, $article->text, $matches, PREG_SET_ORDER);
+
+            foreach ($matches as $match) {
+                    // $match[0] is full pattern match, $match[1] is the url
+                    $code = $this->_getDownloadHtml($match[1]);
+                    // We should replace only first occurrence in order to allow positions with the same name to regenerate their content:
+                    $article->text = preg_replace("|$match[0]|", $code, $article->text, 1);
             }
-            return $newstring;
+            return '';
+    }
+
+    /*
+     * 
+     */
+    private function _getDownloadHtml($path){
+
+    }
+        
+    /*
+     * Parse directory in to one object, recursively
+     * @todo: think if is really a good ideal parse a tree of directories instead
+     * of just one directory
+     * 
+     * @return      object
+     */
+    private function _parseDirectoryTree( $directory = NULL )
+    {
+        $directory = $this->_clearDirectory( $directory );
+
+        if(!$directory){
+            return FALSE;
         }
+
+        $info = array();
+        $dir = opendir( $directory );
+        while ( ( $filePointer = readdir($dir)) !== FALSE )
+        {
+            if( $filePointer != '.' && $filePointer != '..' )
+            {
+                $path = $directory .'/'.$filePointer;
+                if( is_readable($path) )//Be sure if file or path is readable before try parse it
+                {
+                    $subdirs = explode('/',$path);
+                    if(is_dir($path))
+                    {
+                        $info[] = array(
+                            'path'          => $path,
+                            'type'          => 'directory',
+                            'name'          => end($subdirs),
+                            'permission'    => substr( decoct( fileperms($path) ), 1),
+                            'content'       => $this->_parseDirectoryTree( $path )
+                        );
+                    }elseif(is_file($path))
+                    {
+                        $ext = substr( strrchr( end($subdirs),'.' ),1 );
+                        $info[] = array(
+                            'path'          => $path,
+                            'type'          => $ext,
+                            'name'          => end($subdirs),
+                            'edited'        => date ("Y:m:d H:i:s", filemtime($path)),
+                            'size'          => filesize($path)
+                        );
+                    }
+                }
+            }
+        }
+        closedir($dir);
+        return $info;
+    }
+        
 }
--- readme.txt
+++ readme.txt
@@ -1,27 +1,23 @@
-Joomla 1.6 Content Plugin GetGithubCode
-Import github code on the fly and insert on your articles
-
-Author: Emerson Rocha Luiz - @fititnt - emerson@webdesign.eng.br
-
+PHP Joomla 1.6+ Module
+PHP | Joomla | Module to show contents of one directory
 
 ------- Basic usage -------
-1. Install the plugin. Joomla 1.6+ requered
-2. Publish the plugin
-3. Add tags whith link to RAW page that you want to inset, like the next example:
-    {githug}https://raw.github.com/fititnt/plg_getgithubcode/master/getgithubcode.php{/github}
 
 ------- Important -------
-* You can use more plugins to edit aparence of code by puttin this plugin before and, editing the start and end tags of generated code.
-* For now, this plugin does not have cache enabled. Think about to use Joomla Cache for avoid access all times the github to show your code.
 
--------   @todo   -------
-* Handle better Win linebreaksl* Implement on regex take just some lines of file
+-------   Todo    -------
++ Create a mod_directorydownload
 
 ------- Changelog -------
+2011-09-20: 0.2alpha
 
-2011-07-15: 0.2beta2
-- Fixed linebreaks problem. (@todo: need check on win and mac OSs)
-- Fixed parameters get
+2011-07-18: 0.1alpha
+! Created. Based on plg_getgithubcode
 
-2011-07-15: 0.2beta1
-- Changelog start
+CHANGELOG LEGEND:
++ Added
+- Removed
+^ Updated
+* Bugfix
+# Security Fix
+! Relevant message
